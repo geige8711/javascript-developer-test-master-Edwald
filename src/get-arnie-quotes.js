b7bcc67492b0e2190c267a57daaac8413c402ddf
Edwald He <edwald.he@gmail.com>
2021-11-17 18:41:33 +1100

    finish unit test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,18 @@
 const { httpGet } = require('./mock-http-interface')
 
 const getArnieQuotes = async (urls) => {
-    // TODO: Implement this function.
-    // return results;
+    return Promise.all(
+        urls.map(async (url) => {
+            try {
+                const response = await httpGet(url)
+                const resKey =
+                    response.status === 200 ? 'Arnie Quote' : 'FAILURE'
+                return { [resKey]: JSON.parse(response.body).message }
+            } catch (err) {
+                console.log('Should handle error here', err)
+            }
+        })
+    )
 }
 
 module.exports = {
